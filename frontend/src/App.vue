e347c227195148f74e545d38c4aaa1c8566bd434
Lisa Wagner <wagnerl@mskcc.org>
2020-11-11 09:37:16 -0500

    Navigation Bar
    - changed from amterial tabs to diy
    - added tools menu
    - selected tool highlights parent menu when active
    - added mskcc color vars
    - changed print width to 140

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,72 +1,61 @@
 <template>
-  <div id="app" v-cloak>
-    <div class="container">
-      <!-- <nav class="header">
-        <ul class="nav-list">
-          <li v-for="link in links" :key="link" class="nav-links">
-            <router-link exact :to="{ name: link }">{{ link }}</router-link>
-          </li>
-        </ul>
-      </nav> -->
-      <div class="header">
-        <md-tabs md-sync-route class="nav-links">
-          <md-tab
-            exact
-            v-for="link in links"
-            :key="link"
-            :to="{ name: link }"
-            :md-label="link"
-          ></md-tab>
-          <!-- <md-tab id="my-tools"></md-tab> -->
-        </md-tabs>
-        <md-menu md-size="medium" :md-offset-x="127" :md-offset-y="-36">
-          <md-button md-menu-trigger>My Tools</md-button>
-          <md-menu-content>
-            <md-menu-item>My Item 1</md-menu-item>
-            <md-menu-item>My Item 2</md-menu-item>
-            <md-menu-item>My Item 3</md-menu-item>
-          </md-menu-content>
-        </md-menu>
-        <img
-          class="igo-logo"
-          alt="IGO logo"
-          src="./assets/logoWhiteOnTransp.png"
-        />
-      </div>
+    <div id="app" v-cloak>
+        <div class="container">
+            <div class="header">
+                <div class="nav">
+                    <router-link exact v-for="link in links" :key="link" :to="{ name: link }" class="nav-link">
+                        <md-button class="md-ripple nav-button">{{ link }}</md-button></router-link
+                    >
+                    <md-menu md-size="medium" md-align-trigger class="nav-link" v-bind:class="{ 'router-link-active': toolsIsActive }">
+                        <md-button md-menu-trigger class="md-ripple nav-button">Tools</md-button>
+                        <md-menu-content>
+                            <md-menu-item v-for="toolLink in toolLinks" :key="toolLink"
+                                ><router-link :to="{ name: toolLink }" class="nav-link">
+                                    <md-button class="md-ripple nav-button">{{ toolLink }}</md-button></router-link
+                                ></md-menu-item
+                            >
+                        </md-menu-content></md-menu
+                    >
+                </div>
 
-      <div class="main">
-        <router-view></router-view>
-      </div>
-      <div class="footer">
-        <img class="msk-logo" alt="MSK logo" src="./assets/icon2.png" />
-      </div>
+                <img class="igo-logo" alt="IGO logo" src="./assets/logoWhiteOnTransp.png" />
+            </div>
+
+            <div class="main">
+                <router-view></router-view>
+            </div>
+            <div class="footer">
+                <img class="msk-logo" alt="MSK logo" src="./assets/icon2.png" />
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "App",
-  // components: {
-  //   // HelloWorld,
-  //   HomePage,
-  // },
-  data: function() {
-    return {
-      links: [
-        "home",
-        "about",
-        "services and prices",
-        "samples",
-        "contacts",
-        "faqs",
-        "criteria",
-      ],
-    };
-  },
+    name: 'App',
+    // components: {
+    //   // HelloWorld,
+    //   HomePage,
+    // },
+
+    data: function() {
+        return {
+            links: ['home', 'about', 'services and prices', 'samples', 'contacts', 'faqs'],
+            toolLinks: ['criteria', 'about', 'services and prices'],
+            toolsIsActive: ['criteria', 'about', 'services and prices'].includes(this.$route.name),
+        };
+    },
+    watch: {
+        $route() {
+            this.toolsIsActive = this.toolLinks.includes(this.$route.name);
+            console.log(this.toolsIsActive);
+            console.log(this.$route.name);
+        },
+    },
 };
 </script>
 
 <style lang="css">
-@import "./assets/css/style.css";
+@import './assets/css/style.css';
 </style>
