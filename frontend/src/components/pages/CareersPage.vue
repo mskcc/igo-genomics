55174c75b0cdf389dcf3e15a33ebee7c21cd8b1c
anna patruno <apatruno618@gmail.com>
2021-03-31 17:01:26 -0400

    Careers Page
    - added link in top nav bar
    - empty state message

---
+++ frontend/src/components/pages/CareersPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <md-empty-state md-rounded md-icon="work" md-label="We're fully staffed!" md-description="Check back for any openings!">
+    </md-empty-state>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
