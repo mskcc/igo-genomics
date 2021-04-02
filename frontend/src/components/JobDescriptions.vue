7e19adbe211a60fef33c9446676e786a6cb11d45
anna patruno <apatruno618@gmail.com>
2021-04-02 14:05:05 -0400

    JobDescriptions component
    - if no job openings show md-empty-state else show job-descriptions

---
+++ frontend/src/components/JobDescriptions.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <md-card class="news-card">
+      <md-card-header>
+        <div class="md-title">R&D Manager</div>
+        <div class="md-subhead">Research and Development Team</div>
+      </md-card-header>
+
+      <md-card-actions>
+        <md-button @click="showJobDescription = true">Learn More</md-button>
+      </md-card-actions>
+    </md-card>
+    <md-dialog :md-active.sync="showJobDescription">
+      <!-- <md-dialog-title>Job Description</md-dialog-title> -->
+
+      <md-tabs md-dynamic-height>
+        <md-tab md-label="General">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam mollitia dolorum dolores quae commodi impedit possimus qui,
+            atque at voluptates cupiditate. Neque quae culpa suscipit praesentium inventore ducimus ipsa aut.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam mollitia dolorum dolores quae commodi impedit possimus qui,
+            atque at voluptates cupiditate. Neque quae culpa suscipit praesentium inventore ducimus ipsa aut.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam mollitia dolorum dolores quae commodi impedit possimus qui,
+            atque at voluptates cupiditate. Neque quae culpa suscipit praesentium inventore ducimus ipsa aut.
+          </p>
+        </md-tab>
+      </md-tabs>
+
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="showJobDescription = false">Close</md-button>
+        <md-button class="md-primary" @click="showJobDescription = false">Save</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JobDescriptions',
+  data: function() {
+    return {
+      showJobDescription: false,
+    };
+  },
+};
+</script>
+
+<style></style>
--- frontend/src/components/pages/CareersPage.vue
+++ frontend/src/components/pages/CareersPage.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <md-empty-state md-rounded md-icon="work" md-label="We're fully staffed!" md-description="Check back for any openings!">
-    </md-empty-state>
+    <md-empty-state
+      v-if="noJobs"
+      md-rounded
+      md-icon="work"
+      md-label="We're fully staffed!"
+      md-description="Check back for openings!"
+    ></md-empty-state>
+    <job-descriptions v-else></job-descriptions>
   </div>
 </template>
 
 <script>
-export default {};
+import JobDescriptions from '../JobDescriptions.vue';
+export default {
+  components: { JobDescriptions },
+  data: function() {
+    return {
+      noJobs: false,
+    };
+  },
+};
 </script>
 
 <style></style>
