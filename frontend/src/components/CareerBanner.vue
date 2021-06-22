7215fa7aac516fb251b4c370f9a9426c135a21b0
anna patruno <apatruno618@gmail.com>
2021-06-22 16:01:39 -0400

    Careers page photo banner

---
+++ frontend/src/components/CareerBanner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="career-banner md-layout ">
+    <img class="md-layout-item" src="../assets/images/lab/IGO_210408_032.jpg" />
+    <img class="md-layout-item" src="../assets/images/lab/IGO_210408_153.jpg" />
+    <img class="md-layout-item" src="../assets/images/lab/IGO_210408_160.jpg" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
