80fbe6376bddd6e0a66dd73f90fd9da485b34369
anna patruno <apatruno618@gmail.com>
2022-02-23 10:38:46 -0500

    Reservations menu instead of page, highlight total cost of targeted sequencing

--- frontend/src/components/pages/ServicesPricesPage.vue
+++ frontend/src/components/pages/ServicesPricesPage.vue
@@ -93,7 +93,7 @@
                         <md-table-head md-numeric v-if="service.tableHeaders && service.tableHeaders.includes('sampleCostFfpe')">Cost Per Sample (PCR+)</md-table-head>
                         <md-table-head md-numeric v-if="service.tableHeaders && service.tableHeaders.includes('sampleCostWet')">Wet Lab Cost</md-table-head>
                         <md-table-head md-numeric v-if="service.tableHeaders && service.tableHeaders.includes('sampleCostDry')">Dry Lab Cost</md-table-head>
-                        <md-table-head md-numeric v-if="service.tableHeaders && service.tableHeaders.includes('totalCost')">Total Cost</md-table-head>
+                        <md-table-head md-numeric v-if="service.tableHeaders && service.tableHeaders.includes('totalCost')" class="highlight-column">Total Cost</md-table-head>
                         <md-table-head v-if="service.tableHeaders && service.tableHeaders.includes('platform')">Platform</md-table-head>
                         <md-table-head v-if="service.tableHeaders && service.tableHeaders.includes('sequencingDetails')"
                           >Sequencing Details</md-table-head
@@ -134,7 +134,7 @@
                         >
                         <md-table-cell md-numeric v-if="service.tableHeaders && service.tableHeaders.includes('sampleCostWet')">${{ row.sampleCostWet }}</md-table-cell>
                         <md-table-cell md-numeric v-if="service.tableHeaders && service.tableHeaders.includes('sampleCostDry')">${{ row.sampleCostDry }}</md-table-cell>
-                        <md-table-cell md-numeric v-if="service.tableHeaders && service.tableHeaders.includes('totalCost')">${{ row.totalCost }}</md-table-cell>
+                        <md-table-cell md-numeric v-if="service.tableHeaders && service.tableHeaders.includes('totalCost')" class="highlight-column">${{ row.totalCost }}</md-table-cell>
                         <md-table-cell v-if="service.tableHeaders && service.tableHeaders.includes('platform')">{{
                           row.platform
                         }}</md-table-cell>
