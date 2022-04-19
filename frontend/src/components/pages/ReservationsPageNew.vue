97bffd3d3955014692b745d71e881d12518633ef
Stephanie DelBelso <steph.lauren19@gmail.com>
2022-04-19 15:52:30 -0400

    update links

--- frontend/src/components/pages/ReservationsPageNew.vue
+++ frontend/src/components/pages/ReservationsPageNew.vue
@@ -3,14 +3,34 @@
     <div class="md-display-1">Reservations</div>
     <div class="reservation-buttons-container">
         <div class="reservations-button">
-            <strong><a :href="spmCalendar" target="__blank"><i class="far fa-box-open"></i> Container Pickup</a></strong> 
+            <a :href="spmCalendar" target="__blank">
+                <div>
+                    <i class="far fa-box-open reservations-button-icon"></i>
+                </div>
+                <div class="md-title">
+                    Container Pickup
+                </div>
+            </a>
         </div>
         <div class="reservations-button">
-            <strong><a :href="spmCalendar" target="__blank"><i class="fas fa-vials"></i> Sample Drop Off</a></strong> 
-
+            <a :href="spmCalendar" target="__blank">
+                <div>
+                    <i class="fas fa-vials reservations-button-icon"></i>
+                </div>
+                <div class="md-title">
+                    Sample Drop Off
+                </div>
+            </a>
         </div>
         <div class="reservations-button">
-            <strong><a :href="scCalendar" target="__blank"><i class="fas fa-vial"></i> Fresh Single Cell</a></strong> 
+            <a :href="scCalendar" target="__blank">
+                <div>
+                    <i class="fas fa-vial reservations-button-icon"></i>
+                </div>
+                <div class="md-title">
+                    Fresh Single Cell
+                </div>
+            </a>
         </div>
     </div>
   </div>
