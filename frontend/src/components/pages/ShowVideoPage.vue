843179c58c249461434c11291bcca3ba288017d2
Stephanie DelBelso <sdelbelso@grubhub.com>
2023-06-16 09:57:35 -0400

    Remove Igo at Home title (#119)

--- frontend/src/components/pages/ShowVideoPage.vue
+++ frontend/src/components/pages/ShowVideoPage.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <div class="md-display-1">IGO At Home</div>
     <vue-core-video-player :src="'https://igo.mskcc.org/media/webinars_2020/' + name + '.mp4'"></vue-core-video-player>
   </div>
 </template>
